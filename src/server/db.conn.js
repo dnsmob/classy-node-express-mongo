6fa58d42f63a77a052fd6101c0efa38df56fbeda
dns sandas <eurodns@hotmail.com>
2016-10-24 22:55:30 +0100

    added db listener

--- src/server/db.conn.js
+++ src/server/db.conn.js
@@ -8,5 +8,10 @@ var dbServer = 'localhost:27017';
 var conn = `mongodb://${dbServer}/${dbName}`;
 mongoose.connect(conn);
 
-
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  // we're connected!
+  console.log('we\'re in.');
+});
 
